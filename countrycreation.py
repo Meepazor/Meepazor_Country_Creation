40c22d8b69d981f6343d69d4083b3d373aac8121
Meepazor <166954541+Meepazor@users.noreply.github.com>
2025-01-03 23:43:53 +0000

    Character Creator

--- countrycreation.py
+++ countrycreation.py
@@ -24,10 +24,72 @@ def remove_last_line(file):
 def substring_after(string, delimiter):
     return (string.partition(delimiter)[-1]).replace(' ','').replace('\n','')
 
+def find_mod_folder():
+    global mod_folder_location, base_path
+    if not os.path.exists(os.path.dirname(base_path)+"common"):
+        tkinter.Tk().withdraw()
+        mod_folder_location = os.path.dirname(filedialog.askdirectory(initialdir = "/",title = "Select the common folder from the your mod"))
+    else:
+        mod_folder_location = base_path
+
+def find_mod_file(file_path, initial_text, delete_last_num=False):
+    
+    os.makedirs(os.path.dirname(mod_folder_location+file_path),exist_ok=True)
+    if os.path.exists(mod_folder_location+file_path):
+        with open(mod_folder_location+file_path, "r") as file:
+            first = file.read(1)
+        
+        with open(mod_folder_location+file_path, "a") as file:
+            if not first:
+                file.write(initial_text)
+            else:
+                file.write("\n")
+                if not delete_last_num == False:
+                    for x in range(0,delete_last_num):
+                        remove_last_line(mod_folder_location+file_path)
+
+def read_without_comments(file, comments=0):
+    with open(file,'r') as file_read:
+        array = file_read.readlines()
+    array = array[comments:]
+    print(array)
+    return array
+            
+def create_character(tag_input, character_input, ideology_input):
+    global ideologies, ideologies_sub, ideologies_full, mod_folder_location, history_folder_location
+    
+    find_mod_file('/common/characters/'+tag_input+'_characters.txt','characters = {\n',1)
+    find_mod_file('/localisation/english/'+tag_input+'_l_english.yml','l_english \n ')
+    
+    character_input_tag = tag_input+'_'+(character_input.replace(' ', '_').lower())
+    
+    if ideology_input in ideologies:
+        ideology_tag = ideologies_sub[ideologies.index(ideology_input)]
+    elif ideology_input in ideologies_full:
+        ideology_tag = ideologies_sub[ideologies_full.index(ideology_input)]
+    else:
+        ideology_tag = ideology_input
+        
+    with open(mod_folder_location+'/common/characters/'+tag_input+'_characters.txt', "a") as character_file:
+        character_file.write('\t'+character_input_tag+' = { \n\t\tname = '+character_input_tag+'\n\t\tportraits = {\n\t\t\tcivilian = {\n\t\t\t\tlarge = GFX_portrait_'+character_input_tag+'\n\t\t\t}\n\t\t}\n\t\tcountry_leader = {\n\t\t\tideology = '+ideology_tag+'\n\t\t\texpire = "1960.1.1.1"\n\t\t\tid = -1\n\t\t}\n\t}\n}')
+    
+    for files in os.listdir(mod_folder_location+'/history/countries'):
+        if (os.path.basename(files))[:3] == tag_input:
+            history_folder_location = mod_folder_location+'/history/countries/'+files
+    
+    with open(history_folder_location, "r") as history_file:
+        history_file_data = history_file.read()
+        history_file_data = history_file_data.replace('set_politics', 'recruit_character = '+character_input_tag+'\nset_politics')
+    with open(history_folder_location, "w") as history_file:
+        history_file.write(history_file_data)
+    
+    with open(mod_folder_location+'/localisation/english/'+tag_input+'_l_english.yml', "a") as loc_file:
+        loc_file.write('\n '+character_input_tag+': "'+character_input+'"\n '+character_input_tag+'_desc: "'+character_input+' Description"')
+
 #stuff needed later on
 base_path = pathlib.Path(__file__).parent.resolve()
 mode_val = '99'
-available_modes = ['0','1','2','3']
+available_modes = ['0','1','2','3','4']
 character_input = ''
 create_flag_ind = ''
 create_mult_flag_ind = ''
@@ -51,7 +113,7 @@ ideologies_sub = ['conservatism', 'marxism', 'gen_nazism', 'despotism']
 
 #option to create folders, keeps going until you actually answer y or n
 while not mode_val in available_modes:
-    mode_val = str(input("1 - Full Country Creation\n2 - Flag Resizing\n3 - Focus Tree Supplementor\n0 - Exit \nWhich mode would you like? "))
+    mode_val = str(input("1 - Full Country Creation\n2 - Flag Resizing\n3 - Focus Tree Supplementor\n4 - Character Creator\n0 - Exit \nWhich mode would you like? "))
 
 #various paths to folders needed
 path_array = ["common/country_tags","common/countries","common/characters","history/countries","history/units","localisation/english","gfx/flags/medium","gfx/flags/small","history/states"]
@@ -375,84 +437,128 @@ elif mode_val == '3':
     
     options_array = ["tag = ","add_ideas"]
     tag_found_ind = False
-    old_idea_ind = False
-    
-    ideas_array = []
-    ideas_bracket_array = []
-    ideas_contents = ["\nideas = {\n\tcountry = {\n\t"," = {\n\t\t\t","picture = generic_economic_increase\n\n\t\t\tallowed = { always = no }\n\t\t\tallowed_civil_war = { always = no }\n\t\t\tremoval_cost = -1\n\n\t\t\tmodifier = {}\n\t\t}\n\t","}\n}"]
-    
+    supp_ideas_ind = ''
+
     with open(focus_input, "r") as focus_file:
         focus_file_data = focus_file.readlines()
-    
+
     for line in focus_file_data:
         if tag_found_ind == False and options_array[0] in line:
             tag_input = substring_after(line, '=')
             tag_found_ind = True
-        elif options_array[1] in line:
-            if '{' in line:
-                a = 1
-            elif substring_after(line, '=') not in ideas_array:
-                ideas_array.append(substring_after(line, '='))
     
-    if os.path.exists("saved_additions_"+mod_name+".txt"):
-        with open("saved_additions_"+mod_name+".txt", "r") as save_file:
-            save_file_data = save_file.readlines()
-            for old_idea_orig in save_file_data:
-                old_idea = old_idea_orig.replace('IDEA:','').replace('\n','')
-                if old_idea in ideas_array:
-                    ideas_array.remove(old_idea)
-                    old_idea_ind = True
-                    
-    if ideas_array:
-        os.makedirs(common_folder_location+"/ideas/",exist_ok=True)
-        os.makedirs(os.path.dirname(common_folder_location)+"/localisation/english/",exist_ok=True)
+    #choose whether you want to do ideas, keeps going until you enter y or n
+    while not (supp_ideas_ind =='y' or supp_ideas_ind == 'n'):
+        supp_ideas_ind = input("Ideas (y/n)? ")
+    
+    if supp_ideas_ind == 'y':
+    
+        ideas_array = []
+        ideas_bracket_array = []
+        ideas_contents = ["\nideas = {\n\tcountry = {\n\t"," = {\n\t\t\t","picture = generic_economic_increase\n\n\t\t\tallowed = { always = no }\n\t\t\tallowed_civil_war = { always = no }\n\t\t\tremoval_cost = -1\n\n\t\t\tmodifier = {}\n\t\t}\n\t","}\n}"]
+        old_idea_ind = False
         
-        if old_idea_ind == True:
-            remove_last_line(common_folder_location+"/ideas/"+tag_input+"_ideas.txt")
-            remove_last_line(common_folder_location+"/ideas/"+tag_input+"_ideas.txt")
+        for line in focus_file_data:
+            if options_array[1] in line:
+                if '{' in line:
+                    a = 1
+                elif substring_after(line, '=') not in ideas_array:
+                    ideas_array.append(substring_after(line, '='))
         
-        with open(common_folder_location+"/ideas/"+tag_input+"_ideas.txt", "a") as idea_file:
-            if old_idea_ind == True:
-                idea_file.write("\t")
-            else:
-                idea_file.write(ideas_contents[0])
-                     
-            for idea in ideas_array:
-                idea_file.write("\t"+idea+ideas_contents[1]+ideas_contents[2])
-            idea_file.write(ideas_contents[3])
-        
-        with open(os.path.dirname(common_folder_location)+"/localisation/english/"+tag_input+"_l_english.yml", "r") as save_file:
-            first_char = save_file.read(1)
+        if os.path.exists("saved_additions_"+mod_name+".txt"):
+            with open("saved_additions_"+mod_name+".txt", "r") as save_file:
+                save_file_data = save_file.readlines()
+                for old_idea_orig in save_file_data:
+                    old_idea = old_idea_orig.replace('IDEA:','').replace('\n','')
+                    if old_idea in ideas_array:
+                        ideas_array.remove(old_idea)
+                        old_idea_ind = True
+                        
+        if ideas_array:
+            os.makedirs(common_folder_location+"/ideas/",exist_ok=True)
+            os.makedirs(os.path.dirname(common_folder_location)+"/localisation/english/",exist_ok=True)
             
-        with open(os.path.dirname(common_folder_location)+"/localisation/english/"+tag_input+"_l_english.yml", "a", encoding='utf-8-sig') as localisation_file:
-            if not first_char:
-                localisation_file.write("l_english:")
-            else:
-                localisation_file.write("\n")
-                
-            for idea in ideas_array:
-                if idea[:4] == tag_input+'_':
-                    idea_proper = idea.replace(tag_input+'_', '').replace('_',' ').title()
+            if old_idea_ind == True:
+                remove_last_line(common_folder_location+"/ideas/"+tag_input+"_ideas.txt")
+                remove_last_line(common_folder_location+"/ideas/"+tag_input+"_ideas.txt")
+            
+            with open(common_folder_location+"/ideas/"+tag_input+"_ideas.txt", "a") as idea_file:
+                if old_idea_ind == True:
+                    idea_file.write("\t")
                 else:
-                    idea_proper = idea.replace('_',' ').title()
-                localisation_file.write('\n '+idea+': "'+idea_proper+'"')
-                localisation_file.write('\n '+idea+'_desc: "'+idea_proper+' Description"')
-        
-        if not os.path.exists("saved_additions_"+mod_name+".txt"):
-            save_file = open("saved_additions_"+mod_name+".txt", "w")
-            save_file.close()
-                 
-        with open("saved_additions_"+mod_name+".txt", "r") as save_file:
-            first_char = save_file.read(1)
-        
-        print("\n")
-        with open("saved_additions_"+mod_name+".txt", "a") as save_file:    
-            if not first_char:
-                save_file.write("#Additions already made by the tool for the mod: "+mod_name+"\n#This avoids duplication so don't remove this or any lines from it!")
-            for idea in ideas_array:
-                print("Added idea "+idea+" to '"+tag_input+"_ideas.txt' and '"+tag_input+"_l_english.yml'")
-                save_file.write("\nIDEA:"+idea)
+                    idea_file.write(ideas_contents[0])
+                         
+                for idea in ideas_array:
+                    idea_file.write("\t"+idea+ideas_contents[1]+ideas_contents[2])
+                idea_file.write(ideas_contents[3])
+            
+            with open(os.path.dirname(common_folder_location)+"/localisation/english/"+tag_input+"_l_english.yml", "r") as localisation_file:
+                first_char_loc = localisation_file.read(1)
+                
+            with open(os.path.dirname(common_folder_location)+"/localisation/english/"+tag_input+"_l_english.yml", "a", encoding='utf-8-sig') as localisation_file:
+                if not first_char_loc:
+                    localisation_file.write("l_english:")
+                else:
+                    localisation_file.write("\n")
+                    
+                for idea in ideas_array:
+                    if idea[:4] == tag_input+'_':
+                        idea_proper = idea.replace(tag_input+'_', '').replace('_',' ').title()
+                    else:
+                        idea_proper = idea.replace('_',' ').title()
+                    localisation_file.write('\n '+idea+': "'+idea_proper+'"')
+                    localisation_file.write('\n '+idea+'_desc: "'+idea_proper+' Description"')
+            
+            if not os.path.exists("saved_additions_"+mod_name+".txt"):
+                save_file = open("saved_additions_"+mod_name+".txt", "w")
+                save_file.close()
+                     
+            with open("saved_additions_"+mod_name+".txt", "r") as save_file:
+                first_char_save = save_file.read(1)
+            
+            with open("saved_additions_"+mod_name+".txt", "a") as save_file:    
+                if not first_char_save:
+                    save_file.write("#Additions already made by the tool for the mod: "+mod_name+"\n#This avoids duplication so don't remove this or any lines from it!")
+                for idea in ideas_array:
+                    print("Added idea "+idea+" to '"+tag_input+"_ideas.txt' and '"+tag_input+"_l_english.yml'")
+                    save_file.write("\nIDEA:"+idea)
 
+elif mode_val == '4':
+    character_mass_ind = ''
+    
+    while not (character_mass_ind =='1' or character_mass_ind == '2'):
+        character_mass_ind = input("Create a single character (1) or import character_creator.txt (2)? ")
+
+    if character_mass_ind == '1':
+        #choose a tag, keeps going until you input a three character tag where the first is a letter
+        while not len(tag_input)==3 or not tag_input[0:1].isalpha():
+            tag_input = str(input("Choose a tag to recruit the character: ")).upper()
+            
+        #choose a character name, keeps going until you actually enter something
+        while not len(character_input)>0:
+            character_input = str(input("Enter the name of your character: "))
+            
+        #choose an ideology
+        while not len(ideology_input)>0:
+            ideology_input = str(input("Enter your ideology or sub-ideology (anything not recognised as an ideology will be assumed to be a sub-ideology): ")).lower()
+    
+        find_mod_folder()
+        create_character(tag_input, character_input, ideology_input)
+    
+    if character_mass_ind == '2':
+        file_input = filedialog.askopenfilename(initialdir = '/',title = 'Select your character creator file',filetypes = (("Text files","*.txt*"),('All files', '*.*')))
+        characters = read_without_comments(file_input, 3)
+        find_mod_folder()
+        
+        for character in characters:
+            character_info = character.split(';')
+            print(character_info)
+            
+            tag_input = character_info[0]
+            character_input = character_info[1]
+            ideology_input = character_info[2].replace('\n','')
+            create_character(tag_input, character_input, ideology_input)
+        
 close = input("\nDone? ")
 
 
